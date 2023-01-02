6ddde3f1d8dc63db5b07a2fc1680210bd3aa73e6
Ivan Rubio <smashedpurse@gmail.com>
2023-01-02 17:57:59 -0600

    develope the sintaxis of the bucle do-while

--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,20 @@ public class Main {
 
         int contador = 0;
 
+        //BUCLE WHILE
         while(contador <= 10){
-
-
             System.out.println("El valor del contador es: " + contador);
             contador++;
-
         }
+
+        contador = 0;
+
+        //BUCLE DO-WHILE
+
+        do {
+            System.out.println("El valor del contador con el bucle do-while es: " + contador);
+            contador++;
+        } while (contador < 10);
 
     }
 }
