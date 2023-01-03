4151dbcef1bd280a55700ff0bb0a7a76da216aa3
Ivan Rubio <smashedpurse@gmail.com>
2023-01-02 18:19:06 -0600

    develope the sintaxis of the for bucle

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         int contador = 0;
 
         //BUCLE WHILE
+        System.out.println("WHILE *************");
         while(contador <= 10){
             System.out.println("El valor del contador es: " + contador);
             contador++;
@@ -12,11 +13,17 @@ public class Main {
         contador = 0;
 
         //BUCLE DO-WHILE
-
+        System.out.println("DO WHILE *************");
         do {
             System.out.println("El valor del contador con el bucle do-while es: " + contador);
             contador++;
-        } while (contador < 10);
+        } while (contador <= 10);
+
+        //BUCLE FOR
+        System.out.println("BUCLE FOR");
+        for (int i = 0; i <= 10 ; i++){
+            System.out.println("El valor de nuestra variable i con el bucle for es: "+ i);
+        }
 
     }
 }
